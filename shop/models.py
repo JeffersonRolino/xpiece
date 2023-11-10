e04d63c2df04a1eeba5773fcb8efaaf55ba51edb
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-09 23:14:41 -0300

    Criada as Views para Lista de Produtos e Detalhes do Produto, também adicionei url reversa

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,9 @@ class Category(models.Model):
         ]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -34,6 +38,9 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
 
     def __str__(self):
         return self.name
