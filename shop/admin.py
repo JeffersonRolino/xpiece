ab068a47271da02f9d45029f4d27126f7b9109d3
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-09 22:24:50 -0300

    Registrando models na área administrativa

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}
