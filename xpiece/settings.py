3a27e8ec8e0491dc464a3c4a4b18f53139ffccae
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-10 13:30:50 -0300

    Refatorada localização dos diretórios templates e static

--- xpiece/settings.py
+++ xpiece/settings.py
@@ -47,7 +47,9 @@ ROOT_URLCONF = 'xpiece.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static'),
+]
 
 
 # Pasta para upload de imagens
