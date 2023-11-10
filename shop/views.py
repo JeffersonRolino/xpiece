773dce2a36c9606bea81304df7dd99eb1d5830ac
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-10 09:38:12 -0300

    Adicionado templates e css, ambos testados

--- shop/views.py
+++ shop/views.py
@@ -19,4 +19,4 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug)
-    return render(request, 'shop/product/product_detail.html', {'product': product})
+    return render(request, 'shop/product/detail.html', {'product': product})
