38d45f9b58984aeeef35412af4839a00863cb17d
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-13 19:26:14 -0300

    Criado o App Cart (Carrinho)

--- xpiece/settings.py
+++ xpiece/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configuração do Carrinho de Compras
+CART_SESSION_ID = 'cart'
