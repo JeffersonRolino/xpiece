143254e6f286f3bfaef0cec32feaab438f7b92fc
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-15 10:57:57 -0300

    Registrando models na área administrativa

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'created', 'updated', 'paid']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
