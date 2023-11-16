24d2f72e42f44fb4a3a218379a65b4980e19a899
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-15 23:59:23 -0300

    Criando pedido do cliente (orders)

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+
+from .forms import CreateOrderForm
+from .models import OrderItem
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = CreateOrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'])
+            cart.clear()
+        return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = CreateOrderForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
