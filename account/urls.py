7edcf0ffc034d26b1dea6a8608733936cb9bc748
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-23 17:07:30 -0300

    Adicionada funcionalidade para um cliente criar uma conta

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # https://docs.djangoproject.com/en/4.2/topics/auth/default/
     path('', include("django.contrib.auth.urls")),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
