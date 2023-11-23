d4c23c00c88d45c8d8e761827f98eb3e754b520f
jeffersonrolino <jefferson.rolino@gmail.com>
2023-11-23 20:57:55 -0300

    Adicionada obrigatoriedade de login para realizar pedido

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
@@ -12,8 +13,10 @@ from .models import Order, OrderItem
 from .tasks import order_created
 
 
+@login_required
 def order_create(request):
     cart = Cart(request)
+    user = request.user
     if request.method == 'POST':
         form = CreateOrderForm(request.POST)
         if form.is_valid():
@@ -28,8 +31,8 @@ def order_create(request):
             # Redireciona para pagamento
             return redirect(reverse('payment:process'))
     else:
-        form = CreateOrderForm()
-        return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+        form = CreateOrderForm(initial={'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email })
+        return render(request, 'orders/order/create.html', {'cart': cart, 'form': form, 'user': user})
 
 def order_items_download(request, id):
     order = get_object_or_404(Order, id=id)
